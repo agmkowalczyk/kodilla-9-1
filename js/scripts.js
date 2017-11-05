b36acb9f19753c3ee1254e42e9ebac1e2e582246
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-05 04:31:21 +0100

    dodane uwagi mentora

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,20 @@
 var triangleArea;
 function getTriangleArea( a, h ) {
   if ( a > 0 && h > 0 ) {
-    return triangleArea === (a * h / 2);
+    return a * h / 2;
   } else {
-    console.log( '--Nieprawidłowe dane--' );
+    return '--Nieprawidłowe dane--';
   }
 }
 
-console.log(getTriangleArea( 5, 1 ));
+console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea( 10, 15 );
-var triangle2Area = getTriangleArea( 4, 6 );
-var triangle3Area = getTriangleArea( -3, 7 );
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(4, 6);
+var triangle3Area = getTriangleArea(-3, 7);
 
+console.log('triangle1Area: ', triangle1Area);
+console.log('triangle2Area: ', triangle2Area);
+console.log('triangle3Area: ', triangle3Area);
 
 //https://github.com/jamesallardice/jslint-error-explanations/blob/master/message-articles/return-conditional.md
